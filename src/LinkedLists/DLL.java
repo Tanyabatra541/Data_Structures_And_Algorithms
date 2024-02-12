08e85e77c2f556a060c0a10a49b98a1055e93150
Tanya Batra <tanyabatra@Tanyas-MacBook-Pro.local>
2024-02-11 19:55:06 -0500

    dsa

--- src/LinkedLists/DLL.java
+++ src/LinkedLists/DLL.java
@@ -1,4 +1,81 @@
 package LinkedLists;
 
 public class DLL {
+  private class Node {
+    int value;
+    Node next;
+    Node prev;
+
+    public Node(int value){
+      this.value = value;
+    }
+    public Node(int value, Node next, Node prev) {
+      this.value = value;
+      this.next = next;
+      this.prev = prev;
+    }
+  }
+
+  private Node head;
+
+  public void insertFirst(int value){
+    Node node = new Node(value);
+    node.next = head;
+    node.prev = null;
+    if(head!=null){
+      head.prev = node;
+    }
+    head = node;
+  }
+
+  public void display() {
+    Node last = null;
+    Node node = head;
+    while(node != null){
+      System.out.print(node.value + " -> ");
+      last = node;
+      node = node.next;
+    }
+    System.out.println();
+    while(last != null) {
+      System.out.print(last.value + " -> ");
+      last = last.prev;
+    }
+    System.out.println();
+  }
+
+  public void insertLast(int val) {
+    Node node = new Node(val);
+    Node last = head;
+    if(head == null){
+      node.prev = null;
+      head = node;
+      return;
+    }
+    while(last.next != null) {
+      last = last.next;
+    }
+    last.next = node;
+    node.prev = last;
+    node.next = null;
+  }
+
+  public void insert(int value, int index) {
+    Node newNode = new Node(value);
+    Node temp = head;
+    for(int i=1;i<index;i++){
+      temp = temp.next;
+    }
+    if(temp == null) {
+      System.out.println("Node does not exist.");
+      return;
+    }
+    newNode.next = temp.next;
+    temp.next = newNode;
+    newNode.prev = temp;
+    if(newNode.next!=null){
+      newNode.next.prev = newNode;
+    }
+  }
+
 }
--- src/LinkedLists/Main.java
+++ src/LinkedLists/Main.java
@@ -2,21 +2,31 @@ package LinkedLists;
 
 public class Main {
   public static void main(String[] args) {
-    LL list = new LL();
-    list.insertFirst(2);
-    list.insertFirst(10);
-    list.insertFirst(12);
-    list.insertFirst(20);
-    list.insertLast(1);
-    list.insert(100,1);
-    list.display();
+//    LL list = new LL();
+//    list.insertFirst(2);
+//    list.insertFirst(10);
+//    list.insertFirst(12);
+//    list.insertFirst(20);
+//    list.insertLast(1);
+//    list.insert(100,1);
+//    list.display();
 //    System.out.println(list.deleteFirst());
 //    list.display();
 //    System.out.println(list.deleteLast());
 //    list.display();
 //    System.out.println(list.delete(2));
 //    list.display();
-    System.out.println(list.find(13));
+//    System.out.println(list.find(13));
+//    list.display();
+
+
+    DLL list = new DLL();
+    list.insertFirst(2);
+    list.insertFirst(10);
+    list.insertFirst(12);
+    list.insertFirst(20);
+    list.insertLast(200);
+    list.insert(100, 2);
     list.display();
   }
 }
